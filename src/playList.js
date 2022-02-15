29acffada4abaa7e93d80b9b8eea22a86da9b4a9
Alexgagrun <aleksandr.gargun@gmail.com>
2022-02-15 22:29:47 +0300

    add audio player js

---
+++ src/playList.js
@@ -0,0 +1,28 @@
+const playList = [
+  {
+    title: "Summer Wind",
+    src: "../assets/sounds/Summer Wind.mp3",
+    duration: "03:50",
+  },
+  {
+    title: "Love is Beautiful",
+    src: "../assets/sounds/The Timewriter - Love is Beautiful.mp3",
+    duration: "03:50",
+  },
+  {
+    title: "So Free",
+    src: "../assets/sounds/The Timewriter - So Free.mp3",
+    duration: "03:50",
+  },
+  {
+    title: "Love Trap",
+    src: "../assets/sounds/The Timewriter - Love Trap.mp3",
+    duration: "03:50",
+  },
+  {
+    title: "Demo Track 2",
+    src: "../assets/sounds/Demo Track 2.mp3",
+    duration: "03:50",
+  },
+];
+export default playList;
